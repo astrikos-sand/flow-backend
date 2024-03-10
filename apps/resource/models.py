bec8e22190f65c665136ad3b2e424ca8f4ca6ee3
Anand Kumar <anand_k@cs.iitr.ac.in>
2024-03-10 09:18:41 +0530

    feat: add required fields and fix permissions

--- apps/resource/models.py
+++ apps/resource/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from treebeard.ns_tree import NS_Node
+
+from apps.common.models import BaseModel
 
 from config.strings import MONGODB
 from mongodb import db
 
 
-class ResourceGroup(NS_Node):
-    # TODO: Why the name null is allowed? We are using it in the path
-    name = models.CharField(max_length=255, null=True, blank=True)
+# Validation could not be done at model level because of the way treebeard works
+# Always use serializer to create and update ResourceGroup
+class ResourceGroup(NS_Node, BaseModel):
+    name = models.CharField(max_length=255, db_index=True)
     resource_type = models.CharField(max_length=255)
-
-    # TODO: path should be unique??
 
     """
     use update when moving node from one
@@ -68,5 +70,113 @@ class ResourceGroup(NS_Node):
             {"_id": res["_id"]}, {"$set": {"data": {**current_data, **data}}}
         )
 
+    @staticmethod
+    def validate(
+        name: str | None,
+        name_prefix: str | None,
+        resource_type: str,
+        parent: "ResourceGroup | None",
+    ) -> str:
+
+        if name is None and name_prefix is None:
+            raise ValidationError("either name or name_prefix is required")
+
+        if name is not None and name_prefix is not None:
+            raise ValidationError(
+                "name or name_prefix both can't be defined at the same time"
+            )
+
+        if name is not None:
+            matching_resources = (
+                ResourceGroup.get_root_nodes().filter(
+                    name=name, resource_type=resource_type
+                )
+                if parent is None
+                else parent.get_children().filter(
+                    name=name, resource_type=resource_type
+                )
+            )
+            if matching_resources.count() > 0:
+                raise ValidationError(
+                    "Resource with same name and type already exists at insertion level try using name prefix"
+                )
+        else:
+            matching_resource_count = (
+                ResourceGroup.get_root_nodes().filter(
+                    name__startswith=name_prefix, resource_type=resource_type
+                )
+                if parent is None
+                else parent.get_children().filter(
+                    name__startswith=name_prefix, resource_type=resource_type
+                )
+            ).count()
+
+            name = name_prefix
+            if matching_resource_count > 0:
+                name = f"{name_prefix}-{matching_resource_count}"
+        return name
+
     def __str__(self):
-        return f"{self.name}-{self.resource_type} ({self.path})"
+        return f"{self.name} [ {self.resource_type} ] ( {self.path} )"
+
+
+class ResourcePermission(BaseModel):
+    class Method(models.TextChoices):
+        ALLOW = "ALLOW", "allow"
+        DENY = "DENY", "deny"
+
+    class Action(models.TextChoices):
+        READ = "READ", "read"
+        WRITE = "WRITE", "write"
+        UPDATE = "UPDATE", "update"
+        DELETE = "DELETE", "delete"
+
+    action = models.CharField(choices=Action, max_length=10)
+    method = models.CharField(choices=Method, max_length=10)
+    path = models.CharField(max_length=255, db_index=True)
+    parent_resource = models.ForeignKey(
+        ResourceGroup,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="attached_policies",
+        db_index=True,
+    )
+
+    @property
+    def permission_path(self) -> str:
+        return (
+            f"{self.parent_resource.path}/{self.path}"
+            if self.parent_resource
+            else self.path
+        )
+
+    @property
+    def short_name(self) -> str:
+        return f"{self.method} {self.action} on {self.permission_path}"
+
+    def clean(self):
+
+        # policies with same path and parent_resource should not exist
+        # Optimises checking because many policies can have same path but different parent_resource
+        matching_policy = ResourcePermission.objects.filter(
+            path=self.path, parent_resource=self.parent_resource
+        )
+        if matching_policy.count() > 0:
+            raise ValidationError(
+                "Resource permission with same path and parent_resource already exists"
+            )
+
+        # policies with same permission path should not exist
+        resource_path = self.path.split("/").pop()
+        possible_matching_policy = ResourcePermission.objects.filter(
+            path__endswith=f"/{resource_path}"
+        )
+        for policy in possible_matching_policy:
+            if policy.permission_path == self.permission_path:
+                raise ValidationError(
+                    "Resource permission with same permission path already exists"
+                )
+
+    def __str__(self) -> str:
+        return self.short_name
--- apps/resource/serializers.py
+++ apps/resource/serializers.py
@@ -1,24 +1,50 @@
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 
 from apps.resource.models import ResourceGroup
 
 
 class ResourceGroupSerializer(serializers.ModelSerializer):
-    # TODO: fields which are in NS_tree??
-    data = serializers.JSONField()
+    data = serializers.JSONField(write_only=True)
     path = serializers.ReadOnlyField()
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=ResourceGroup.objects.all(),
+        allow_null=True,
+        write_only=True,
+        required=False,
+    )
+    name_prefix = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = ResourceGroup
-        fields = (
-            "name",
-            "resource_type",
-            "path",
-            "data",
+        fields = ("name", "resource_type", "path", "parent", "data","name_prefix")
+        extra_kwargs = {
+            "name": {"required": False},
+        }
+
+    def validate(self, data: dict):
+        name = data.get("name", None)
+        name_prefix = data.pop("name_prefix", None) # remove name_prefix from data
+        resource_type = data.get("resource_type", None)
+        parent = data.get("parent", None)
+        
+        # raises validation error if object to be created can't have a unique path
+        data["name"] = ResourceGroup.validate(
+            name=name,
+            name_prefix=name_prefix,
+            resource_type=resource_type,
+            parent=parent,
         )
+        return super().validate(data)
 
     def create(self, validated_data: dict):
         data = validated_data.pop("data", None)
-        resource = ResourceGroup.add_root(**validated_data)
+        parent = validated_data.pop("parent", None)
+        resource = (
+            ResourceGroup.add_root(**validated_data)
+            if parent is None
+            else parent.add_child(**validated_data)
+        )
         resource.store_data(data)
         return resource
