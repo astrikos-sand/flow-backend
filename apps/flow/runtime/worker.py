afa1cbb2bbe3fd09d23d8829f90f43fe4e336723
Anand Kumar <anand_k@cs.iitr.ac.in>
2024-03-16 13:44:48 +0530

    feat: basic webhook setup done

--- apps/flow/runtime/worker.py
+++ apps/flow/runtime/worker.py
@@ -4,12 +4,21 @@ import json
 from config.const import WORKER_URL
 from django.core.serializers.json import DjangoJSONEncoder
 
+# type = "Normal" | "Triggered"
 
-def submit_task(nodes: dict):
+
+def submit_task(
+    nodes: dict, data: dict = {}, type: str = "Normal", trigger_id: str = None
+):
+
+    if type == "Triggered" and trigger_id is None:
+        raise Exception("When task type is triggred then trigger_id is required")
+
     print("nodes", nodes, flush=True)
     headers = {"Content-type": "application/json", "Accept": "application/json"}
+    data = {"nodes": nodes, "data": data, "type": type}
     response = requests.post(
-        WORKER_URL, data=json.dumps(nodes, cls=DjangoJSONEncoder), headers=headers
+        WORKER_URL, data=json.dumps(data, cls=DjangoJSONEncoder), headers=headers
     )
     if response.status_code != 200:
         raise response.raise_for_status()
